e0b35b64dbbff63510f8082e9dc0b1f0cd0d5511
Poka Tchoneng Hermann <pokatchoneng@gmail.com>
2019-11-29 17:13:42 +0100

    parametres sandbox integres partout et fonctionnels

--- notifyPage.php
+++ notifyPage.php
@@ -2,18 +2,50 @@
     include('admin/dbcon.php');
 
     require("admin/libs/config.php");
+    /*
+    $link = "";
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $link .= "https";
+    else
+        $link .= "http";
+    
+    $link .= "://";
 
+    $link .= $_SERVER['HTTP_POST'];
+
+    $link .= $_SERVER['REQUEST_URI'];
+
+    echo $link;
+
+    $data = [
+        'command' => $link, 
+        'livre' => '10',
+        'achete' => "non",
+        'prixnumerique' => "1495"
+    ];
+    $sql=$pdo->prepare("INSERT into tbl_commands (command, livre, achete, prixnumerique) VALUES(:command, :livre, :achete, :prixnumerique)");
+
+    if ($sql->execute($data)){
+        echo "enregistré";
+    }else{
+        echo "non enregistré";
+    }
+    */
+
+    
     $rI = $_GET['rI'];
     $rMt = $_GET['rMt'];
     $rDvs = $_GET['rDvs'];
     $rH = $_GET['rH'];
+    //$codeMarchant = "WA197M601771";
+    $codeMarchant = "FY786K4327";
 
     $query = $pdo->prepare("select * from tbl_commands where command=:command");
 
     $query->execute(["command"=>$rI]);
 
     if($row = $query->fetch()){
-        if (strcmp($rMt, $row["prixnumerique"]) == 0 && strcmp($rDvs, "XAF")==0 && strcmp($rH, "WA197M601771")==0){
+        if (strcmp($rMt, $row["prixnumerique"]) == 0 && strcmp($rDvs, "XAF")==0 && strcmp($rH, $codeMarchant)==0){
             $sql = $pdo->prepare("UPDATE tbl_commands SET achete=:achete WHERE command=:command");
             $sql->execute([
                 "achete"=>"oui",
@@ -27,5 +59,5 @@
     }else{
         echo "Pas de commande correspondante.";
     }
-
+    
 
